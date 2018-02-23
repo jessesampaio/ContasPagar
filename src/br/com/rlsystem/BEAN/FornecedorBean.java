6022ce7c6d8e0f3569a39cd4bf4246d57d1e59fb
Jessé Sampaio <jessesampaio.ti@gmail.com>
2018-02-23 00:18:48 -0300

    Finalizado FornecedorBean
    Concluído o FornecedorBean;
    Criação das páginas editar.xhtml e listar.xhtml

--- src/br/com/rlsystem/BEAN/FornecedorBean.java
+++ src/br/com/rlsystem/BEAN/FornecedorBean.java
@@ -1,7 +1,11 @@
 package br.com.rlsystem.BEAN;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 
 import br.com.rlsystem.DAO.Fornecedor;
 import br.com.rlsystem.DAO.FornecedorDAO;
@@ -11,10 +15,12 @@ import br.com.rlsystem.DAO.FornecedorDAO;
 public class FornecedorBean {
 	
 	private Fornecedor fornecedor = new Fornecedor();
+	private DataModel<Fornecedor> fornecedores;
 	
 	public String addFornecedor() {
 		
 		String retorno = "erro";
+		
 		try{
 			FornecedorDAO dao = new FornecedorDAO();
 			dao.Salvar(fornecedor);
@@ -24,15 +30,64 @@ public class FornecedorBean {
 		}
 		return retorno;		
 	}
+	
+	public String deleteFornecedor() {
+		
+		String retorno = "erro";
+		
+		try{
+			FornecedorDAO dao = new FornecedorDAO();
+			dao.Delete(fornecedor);
+			retorno = "listar";
+		}catch(Exception ex){
+			System.out.println("ERRO: " + ex.getMessage()); //Apenas para uso no Debug
+		}
+		return retorno;		
+	}
+	public String updateFornecedor() {
+		
+		String retorno = "erro";
+		
+		try{
+			FornecedorDAO dao = new FornecedorDAO();
+			dao.Update(fornecedor);
+			retorno = "listar";
+		}catch(Exception ex){
+			System.out.println("ERRO: " + ex.getMessage());
+		}
+		return retorno;		
+	}
+	
+	public void selecionarForecedor(){
+		this.fornecedor = fornecedores.getRowData();
+	}
+	
+	public void novoFornecedor(){
+		fornecedor = new Fornecedor();
+	}
 
 	//Getters and Setters
 	public Fornecedor getFornecedor() {
 		return fornecedor;
 	}
-	
 	public void setFornecedor(Fornecedor fornecedor) {
 		this.fornecedor = fornecedor;
 	}
-	
-	
+	public DataModel<Fornecedor> getFornecedores(){
+		FornecedorDAO dao = new FornecedorDAO();
+		
+		try {
+			//Retorna lista de fornecedores
+			List<Fornecedor> lista = dao.GetALL(); 
+			fornecedores = new ListDataModel<Fornecedor>(lista);
+		} catch (Exception e) {			
+		}
+		
+		return fornecedores;		
+	}
+
+	public void setFornecedores(DataModel<Fornecedor> fornecedores) {
+		this.fornecedores = fornecedores;
+	}
+
 }
